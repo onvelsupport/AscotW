ae45845d20cb1be6cbfcef937153876a8d24de6d
onvelsupport <onvelsupport@gmail.com>
2026-06-02 20:37:40 +0100

    Add cart item counter badge

--- store/context_processors.py
+++ store/context_processors.py
@@ -1,4 +1,11 @@
 def cart_count(request):
     cart = request.session.get('cart', {})
-    count = sum(item['quantity'] for item in cart.values())
-    return {'cart_count': count}
+
+    count = sum(
+        item.get('quantity', 0)
+        for item in cart.values()
+    )
+
+    return {
+        'cart_count': count
+    }
